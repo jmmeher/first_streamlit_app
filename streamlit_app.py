38ff8772360454bc11fecf274583befeaf820c2c
jmmeher <143754199+jmmeher@users.noreply.github.com>
2023-09-06 17:17:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,11 +43,12 @@ streamlit.dataframe(my_data_rows)
 
 
 ## Allow user to add a fruit to list
-fruit_choice1 = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('What fruit would you like to add?', fruit_choice1)
+fruit_choice = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('What fruit would you like to add?', fruit_choice)
 import requests
-Add_my_fruit_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice1)
-streamlit.write('Thanks for adding ', fruit_choice1)
+Add_my_fruit_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.write('Thanks for adding ', fruit_choice)
+
 
 
 
