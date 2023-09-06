b1f6bd325342de47da01390dfb0f35c85187480a
jmmeher <143754199+jmmeher@users.noreply.github.com>
2023-09-06 17:39:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLerror
+from urllib.error import URLError
 import streamlit
 streamlit.title("My Mom's New Healthy Diner")
    
