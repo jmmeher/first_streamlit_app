d8f6ade364fb8cafa9092dcf1c9c8e0093f8cce1
jmmeher <143754199+jmmeher@users.noreply.github.com>
2023-09-06 17:32:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
-
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLERROR
 import streamlit
 streamlit.title("My Mom's New Healthy Diner")
    
@@ -11,7 +14,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -22,9 +25,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-import requests
+try :
+   
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+ else:
+                      
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # write your own comment -what does the next line do? 
@@ -32,9 +39,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+except urlerror as e:
+streamlit.error()
+
 streamlit.stop()
 
-import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
@@ -46,7 +56,7 @@ streamlit.dataframe(my_data_rows)
 ## Allow user to add a fruit to list
 fruit_choice = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 #streamlit.write('What fruit would you like to add?', fruit_choice)
-import requests
+
 Add_my_fruit_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.write('Thanks for adding ', fruit_choice)
 
